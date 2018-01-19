ac8d172b5f651e962f0a2de1f06e0eb2e4d2fd28
Alcie <aliciazaludova@gmail.com>
2018-01-19 14:23:49 -0600

    replaced string the with string the large

--- sonnet.js
+++ sonnet.js
@@ -6,21 +6,21 @@
 // Replace all occurances of the string "the" with "a large".
 // Set the content of the sonnet div with the new string.var poem = 
 
-var div = document.getElementById("sonnet");
+var poem = document.getElementById("sonnet");
     // this targets the element that has the ID "sonnet"
-var sonnet = div.innerHTML;
+// var sonnet = div.innerHTML;
     // a variable has been created to hold the contents of the 
 console.log(sonnet);
 
 // find where in the paragraph the word orphans
 
-console.log(sonnet.indexOf("orphans")); // .indexOf finds the first occurance of the string in the (). it doesn't have to be a string. put sonnet var before .indexOf because it needs to know where to look.
+console.log(poem.innerHTML.indexOf("orphans")); // .indexOf finds the first occurance of the string in the (). it doesn't have to be a string. put sonnet var before .indexOf because it needs to know where to look.
 
-console.log(div.length); // not works
+console.log(poem.length); // not works
 
-var phrase = sonnet;
-console.log(phrase.length);  // this works
+console.log(poem.length);  // this works
 
-div.innerHTML = div.innerHTML.replace("winter", "yuletide");
-console.log(newPhrase); 
+poem.innerHTML = poem.innerHTML.replace("winter", "yuletide");
+
+poem.innerHTML = poem.innerHTML.replace(/ the /g, " a large ");
 
